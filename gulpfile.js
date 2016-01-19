fa27790aeb5e1d2102d35ab8d794cb9974f54d4f
Bradley K. Harms <brad.k.harms@gmail.com>
2016-01-18 22:19:26 -0600

    Added projects, better responsive styling

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+var express = require('express');
+
+var FILES = [
+  'index.html',
+  'css/**/*.css',
+  'assets/**/*',
+];
+
+gulp.task('default', function() {
+  var app = express();
+  app.use(express.static('.'));
+  app.listen(3000, '0.0.0.0');
+
+  function reload() {
+    gulp.src(FILES)
+      .pipe(livereload({ start: true }));
+  }
+
+  gulp.watch(FILES, reload);
+  reload();
+});
